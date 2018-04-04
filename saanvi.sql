705186ad17694367411929b2cc116fecc5699f31
gwchhlib <33734907+gwchhlib@users.noreply.github.com>
2018-04-04 00:42:38 -0400

    Add files via upload

---
+++ saanvi.sql
@@ -0,0 +1,24 @@
+/** World Records :
+Most Painted Layers (Michael Carmichael) USA June 2004
+Shortest Woman Alive (Jyoti Kisanji Amge) India December 2011
+Tallest Living Man (Sultan Kosen) Turkey 2009
+Youngest Piece Prize Winner (Malala Yousafzai) 2014
+Most Expensive Fungus (Tuber Magnatum Pico) Not Applicable
+Largest Child Safe Guarding Schooling Lesson (FAITH FOUNDATION, V FORCE MEDIA) India November 2017
+Largest Cricket Lesson (Chance to Shine) United Kingdom July 2017
+Most Hula Hoops Spun Simultaneously (Marawa Ibraham) USA November 2015
+Largest Origami Flower (NINA’S INTERNATIONAL SARL) France October 2016
+Largest Stencil (Pro Walls Iconic) Dubai April 2017
+**/
+CREATE TABLE World_Records (Record TEXT, Name TEXT, Country TEXT, Month TEXT, Year INTEGER );
+INSERT INTO World_Records VALUES ("Most Painted Layers", "Michael Carmichael", "USA", "June", "2004");
+INSERT INTO World_Records VALUES ("Shortest Woman Alive", "Jyoti Kisanji Amge", "India", "Decemeber", "2011");
+INSERT INTO World_Records VALUES ("Tallest Living Man", "Sultan Kosen", "Turkey", "N/A", "2009");
+INSERT INTO World_Records VALUES ("Youngest Living Man", "Malala Yousafzai", "N/A", "N/A", "2014");
+INSERT INTO World_Records VALUES ("Most Expensive Fungus", "Tuber Magnatum Pico", "N/A", "N/A", "N/A");
+INSERT INTO World_Records VALUES ("Largest Child Safe Guarding Schooling Lesson", "FAITH FOUNDATION, V FORCE MEDIA", "India", "November", "2017");
+INSERT INTO World_Records VALUES ("Largest Cricket Lesson", "Chance to Shine", "United Kingdom", "July", "2017");
+INSERT INTO World_Records VAlUES ("Most Hula Hoops Spun Simultaneously", "Marawa Ibraham", "USA", "November", "2015");
+INSERT INTO World_Records VAlUES ("Largest Origami Flower", "NINA’S INTERNATIONAL SARLNINA’S", "France", "October", "2016");
+INSERT INTO World_Records VALUES ("Largest Stencil", "Pro Walls Iconic", "Dubai", "April", "2017");
+SELECT * FROM World_Records;
